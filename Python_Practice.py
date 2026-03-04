1f911721e52a0ff8af64ed52bd441af003631e81
Bhamore <abhishek.bhamore@kochcc.com>
2026-03-04 14:05:51 +0530

    initial commit

---
+++ Python_Practice.py
@@ -0,0 +1,16 @@
+
+'''''
+# Basic python funtion
+
+def test ():
+    print("Hello, World!")
+
+test()
+
+'''
+
+
+def greet(name):
+    print(f"Hello, {name}")
+
+greet(input("What is your name? "))
